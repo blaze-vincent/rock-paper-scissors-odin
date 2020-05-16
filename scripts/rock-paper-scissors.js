16ed91a7bd6323cbb195c5db7c684fe8a15225da
blaze vincent <blaze.vincent@hotmail.com>
2020-05-16 17:13:36 -0500

    Add handleInput and game functions to rock-paper-scissors.js

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -19,3 +19,21 @@ function playRound(playerSelection, computerSelection){
         console.error("invalid input");
     }
 }
+
+function handleInput(input){
+    input = input.toLowerCase();
+    input = input[0].toUpperCase() + input.slice(1);
+    return(input);
+}
+
+function game(){
+    let roundCount = 0;
+    while(roundCount < 5){
+        roundCount++;
+        let playerSelection = handleInput(prompt("Make your move!"));
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
