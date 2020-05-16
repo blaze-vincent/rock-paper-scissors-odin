b712791d9ef15b385a88b4b17fc37ee502f6f92b
blaze vincent <blaze.vincent@hotmail.com>
2020-05-16 16:55:04 -0500

    Add playRound function to scripts/rock-paper-scissors.js

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -2,3 +2,18 @@ function computerPlay(){
     let choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.ceil(Math.random() * 10) % 3];
 }
+
+function playRound(playerSelection, computerSelection){
+    let winnerDict = {
+        "Rock": "Paper",
+        "Paper": "Scissors",
+        "Scissors": "Rock"
+    };
+    if(winnerDict[playerSelection] === computerSelection){
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    } else if(playerSelection === computerSelection){
+        console.log(`Tie! You both chose ${playerSelection}`);
+    } else {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    }
+}
